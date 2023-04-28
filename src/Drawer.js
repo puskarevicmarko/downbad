d2464a2f7091243b8fe4fac8a8b919c83d32dc92
puskarevicmarko <puskarevic.marko@gmail.com>
2023-04-28 19:51:20 -0400

    Your commit message

---
+++ src/Drawer.js
@@ -0,0 +1,155 @@
+import React, { useEffect } from 'react';
+import { CupertinoPane, CupertinoSettings } from 'cupertino-pane';
+
+let drawer;
+
+function Drawer({  buttonData = [], onFlyToButtonClick }) {
+
+  useEffect(() => {
+    
+    const topHeight = 600;
+    const middleHeight = 300;
+    const content = document.querySelector('ion-drawer .content');
+
+    drawer = new CupertinoPane('ion-drawer', {
+      breaks: {
+        top: { enabled: true, height: topHeight },
+        middle: { enabled: true, height: middleHeight },
+        bottom: { enabled: true, offset: 90 },
+      },
+      initialBreak: 'top',
+      buttonDestroy: false,
+      fastSwipeClose: true,
+      dragBy: ['.draggable', 'ion-drawer h1'],
+      events: {
+        onDrag: () => {
+          content.style.height = `${window.screen.height - getPaneTransformY() - content.offsetTop}px`;
+          content.setAttribute('style', 'overflow-y: hidden !important');
+        },
+        onDidPresent: () => {
+          content.setAttribute('style', 'overflow-y: auto !important');
+          content.style.height = `${middleHeight - content.offsetTop}px`;
+        },
+        onTransitionEnd: () => {
+          setTimeout(() => {
+            setHeight();
+          }, 200);
+        },
+      },
+    });
+
+    function setHeight() {
+      content.setAttribute('style', 'overflow-y: auto !important');
+      if (drawer.currentBreak() === 'top') {
+        content.style.height = `${topHeight - content.offsetTop}px`;
+      }
+      if (drawer.currentBreak() === 'middle') {
+        content.style.height = `${middleHeight - content.offsetTop}px`;
+      }
+    }
+
+    function getPaneTransformY() {
+      const translateYRegex = /\.*translateY\((.*)px\)/i;
+      const paneEl = document.querySelector('.pane');
+      return paneEl ? parseFloat(translateYRegex.exec(paneEl.style.transform)[1]) : 0;
+    }
+
+    async function presentDrawer() {
+      drawer.present({ animate: true });
+    }
+
+    async function destroyDrawer() {
+      drawer.destroy({ animate: true });
+    }
+
+    async function hideDrawer() {
+      drawer.hide();
+    }
+
+    async function isHiddenDrawer() {
+      console.log(await drawer.isHidden());
+    }
+
+    async function setTopDrawer() {
+      drawer.moveToBreak('top');
+    }
+
+    async function setMiddleDrawer() {
+      drawer.moveToBreak('middle');
+    }
+
+    async function setBottomDrawer() {
+      drawer.moveToBreak('bottom');
+    }
+
+    async function presentTop() {
+      presentDrawer();
+      setTopDrawer();
+    }
+
+
+    }, []);
+
+
+  return (
+    <ion-drawer>
+      <h1 id="location" className="ion-padding">
+        Heinosities
+      </h1>
+      <div id="button-group" className="py-2 pl-4 flex justify-left items-center space-x-4 overflow-x-auto">
+        
+        <h2 className="flex-shrink-0">Associated: </h2>
+        <button type="button" data-latitude="40.7270179" data-longitude="-73.9830509" data-description="Lucy's" class="flex-shrink-0 bg-yellow-500 py-2 px-4 mx-2 rounded-full" onClick={onFlyToButtonClick}
+
+>Lucy's</button>
+        <div id="tags" className="flex-shrink-0">
+        {buttonData.map((button, index) => (
+          <button
+            key={index}
+            type="button"
+            data-latitude={button.latitude}
+            data-longitude={button.longitude}
+            data-description={button.description}
+            className="flex-shrink-0 bg-yellow-500 py-2 px-4 mx-2 rounded-full"
+            onClick={onFlyToButtonClick}
+          >
+            {button.label}
+          </button>
+        ))}
+      </div>
+      </div>
+      <div className="content" overflow-y>
+        <div id="p1"></div>
+        <div id="p2"></div>
+        <div id="p3"></div>
+        <div id="p4"></div>
+        <div id="p5"></div>
+        <div id="p6"></div>
+        <div id="p7"></div>
+        <div id="p8"></div>
+        <div id="p9"></div>
+        <div id="p10"></div>
+        <div id="p11"></div>
+        <div id="p12"></div>
+        <div id="p13"></div>
+        <div id="p14"></div>
+        <div id="p15"></div>
+        <div id="p16"></div>
+        <div id="p17"></div>
+        <div id="p18"></div>
+        <div id="p19"></div>
+        <div id="p20"></div>
+    </div>
+    </ion-drawer>
+  );
+}
+
+export default Drawer;
+export function presentDrawer() {
+    drawer.present({ animate: true });
+  }
+export function destroyDrawer() {
+    drawer.destroy({ animate: true });
+}
+
+  
---
+++ src/MainContent.js
@@ -0,0 +1,208 @@
+import React, { useEffect, useRef, useState } from 'react';
+import logo from './assets/logo.png';
+import subLogo from './assets/sublogo.svg';
+import axios from 'axios';
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import $ from 'jquery';
+import csv2geojson from 'csv2geojson';
+import Drawer from './Drawer.js';
+import { presentDrawer } from './Drawer.js';
+import { destroyDrawer } from './Drawer.js';
+
+function parseButtons(html) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+  const buttons = doc.querySelectorAll('button');
+  const buttonData = [];
+
+  buttons.forEach(button => {
+    buttonData.push({
+      latitude: button.dataset.latitude,
+      longitude: button.dataset.longitude,
+      description: button.dataset.description,
+      label: button.textContent.trim(),
+    });
+  });
+
+  return buttonData;
+}
+
+const MainContent = () => {
+
+        const mapContainer = useRef(null);
+        const [map, setMap] = useState(null);
+        const [buttonData, setButtonData] = useState([]);
+        const mapRef = useRef(null);
+
+        // Add handleFlyToButtonClick function
+        let handleFlyToButtonClick = (event) => {
+            const latitude = parseFloat(event.target.getAttribute('data-latitude'));
+            const longitude = parseFloat(event.target.getAttribute('data-longitude'));
+            const description = event.target.dataset.description;
+            
+            if (map) {
+              map.flyTo({ center: [longitude, latitude], zoom: 14 });
+            }
+
+            destroyDrawer();
+            new mapboxgl.Popup({
+              closeButton: false,
+              })
+              .setLngLat([longitude, latitude])
+              .setHTML(description)
+              .addTo(mapRef.current);
+          };
+
+        useEffect(() => {
+          if (map) return; // Initialize the map only once
+
+          mapboxgl.accessToken = "pk.eyJ1IjoicHVza2FyZXZpY21hcmtvIiwiYSI6ImNsOGM0ODN2ZzBkaG4zb245MXMyd3o3ZGkifQ.e6UX1du_kGFp5YzHVrnMLw";
+      
+          const mapInstance = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: "mapbox://styles/puskarevicmarko/cl1v306x6006a14s2064gtpmd",
+            center: [-73.991556, 40.744972],
+            zoom: 12,
+            trackResize: false,
+          });
+      
+          mapInstance.on("load", () => {
+            setMap(mapInstance);
+            mapRef.current = mapInstance; // Update mapRef with the latest map instance
+            mapInstance.resize();
+      
+            fetch(
+              "https://docs.google.com/spreadsheets/d/1TJ30712MKFqUTv-EiLfaFQnJ_Gb7qwJBh5U5unOVkRU/gviz/tq?tqx=out:csv&sheet=Sheet1"
+            )
+              .then((response) => response.text())
+              .then((csvData) => {
+                csv2geojson.csv2geojson(
+                  csvData,
+                  {
+                    latfield: "Latitude",
+                    lonfield: "Longitude",
+                    delimiter: ",",
+                  },
+                  (err, data) => {
+                    data.features.forEach((datum) => {
+                      datum.properties.HeinosityIndex = parseInt(
+                        datum.properties.HeinosityIndex
+                      );
+                    });
+      
+                    mapInstance.addSource("places", {
+                      type: "geojson",
+                      data: data,
+                    });
+      
+                    mapInstance.addLayer({
+                      id: "places",
+                      type: "circle",
+                      source: "places",
+                      paint: {
+                        "circle-stroke-color": "#FFCA2A",
+                        "circle-opacity": 1,
+                        "circle-radius": {
+                          stops: [
+                            [10, 1],
+                            [14, 13],
+                          ],
+                        },
+                        "circle-stroke-width": 0,
+                        "circle-color": {
+                          property: "HeinosityIndex",
+                          stops: [
+                            [1, "#f7b731"],
+                            [3, "#FD9A01"],
+                            [5, "#FF2C05"],
+                            [21, "#F00505"],
+                          ],
+                        },
+                      },
+                    });
+      
+                    const popup = new mapboxgl.Popup({
+                      closeButton: false,
+                      closeOnClick: false,
+                    });
+      
+                    mapInstance.on("mouseenter", "places", (e) => {
+                      mapInstance.getCanvas().style.cursor = "pointer";
+      
+                      const coordinates = e.features[0].geometry.coordinates.slice();
+                      const name = e.features[0].properties.Name;
+      
+                      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                        coordinates[0] +=
+                          e.lngLat.lng > coordinates[0] ? 360 : -360;
+                      }
+      
+                      popup.setLngLat(coordinates).setHTML(name).addTo(mapInstance);
+                    });
+      
+                    mapInstance.on("mouseleave", "places", () => {
+                      mapInstance.getCanvas().style.cursor = "";
+                      popup.remove();
+                    });
+                    
+                    mapInstance.on("click", "places", (e) => {
+                      // You can perform additional actions based on the
+   
+                    const feature = e.features[0].properties;
+
+                    // Update the UI elements with the feature data
+                    document.getElementById("location").innerHTML = feature.Name;
+                    //document.getElementById("tags").innerHTML = feature.Tags;
+                    const parsedButtons = parseButtons(feature.Tags);
+                    setButtonData(parsedButtons);
+                    
+                   for (let i = 1; i <= 20; i++) {
+                      const postEl = document.getElementById(`p${i}`);
+                      postEl.innerHTML = feature[`Post${i}`];
+                    }
+
+                    presentDrawer();
+
+                    mapInstance.flyTo({
+                        center: e.features[0].geometry.coordinates
+                        });
+          });
+        }
+      );
+    });
+});
+
+setMap(mapInstance);
+
+// Add event listener to the button
+// Query all buttons with the 'flex-shrink-0' class inside the 'tags' element
+const buttons = document.querySelectorAll("#tags .flex-shrink-0");
+
+// Attach the event listener to each button
+buttons.forEach(button => {
+  button.addEventListener("click", handleFlyToButtonClick);
+});
+}, [map]);
+
+
+return (
+    <>
+      <section>
+        <div className="container px-6 pt-6 mx-auto flex justify-center overflow-auto">
+          <div className="w-full md:w-2/3 lg:w-1/2 full bg-yellow-500 bg-opacity-75 rounded-2xl flex flex-col relative z-10 shadow-2xl overflow-hidden">
+            <div className="flex items-center bg-yellow-500 flex-col items-start px-6 py-5">
+              <img src={logo} className="object-contain md:h-12" alt="Down Bad" />
+              <img src={subLogo} className="object-contain md:h-5" alt="Manhattan's Most Memed" />
+            </div>
+          </div>
+        </div>
+        <div className="absolute inset-0" ref={mapContainer} id="map" />
+      </section>
+      <Drawer buttonData={buttonData} onFlyToButtonClick={handleFlyToButtonClick} />
+    </>
+    
+  );
+};
+
+export default MainContent;
